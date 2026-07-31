742ca08869a2fc31031ce0411ed1f69d383eb884
locantrabusiness-art <locantra.business@gmail.com>
2026-07-31 16:58:25 +0000

    feat: add ImageKit authentication API

--- api/imagekit-auth.js
+++ api/imagekit-auth.js
@@ -1,18 +1,21 @@
 import ImageKit from "imagekit";
 
 const imagekit = new ImageKit({
-  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 });
 
 export default function handler(req, res) {
-  try {
+
+    if (req.method !== "GET") {
+        return res.status(405).json({
+            message: "Method Not Allowed"
+        });
+    }
+
     const authParams = imagekit.getAuthenticationParameters();
+
     res.status(200).json(authParams);
-  } catch (err) {
-    res.status(500).json({
-      error: err.message
-    });
-  }
+
 }
